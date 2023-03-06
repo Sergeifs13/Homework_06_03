622d07522fc7892d5815bf8010e3db920e5c2d97
Sergeifs13 <nossal_av@mail.ru>
2023-03-06 22:26:51 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Student student1 = new Student("Den", 123);
+        Student student2 = new Student("Pen",124);
+        Student student3 = new Student("Fil",125);
+
+    }
+
+}
---
+++ src/Student.java
@@ -0,0 +1,9 @@
+
+public class Student {
+    String name;
+    int number;
+public  Student(String name,int number){
+    System.out.println("New ID created");
+    System.out.println("Name "+name+" Num: "+number);
+}
+}
